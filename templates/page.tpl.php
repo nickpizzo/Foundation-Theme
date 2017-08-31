0dd7e27d2a4bd9e876a11f341450b40a7afa7021
Nick Pizzo <nick.pizzo@gmail.com>
2017-08-31 15:46:10 -0700

    various updates

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,13 +1,18 @@
+<?php drupal_add_library('system', 'ui.accordion'); ?>
 <div class="page-wrap">
 	<header class="header" role="banner">
 
 		<!-- Header Top bar -->
 		<div id="header-top-bar-wrap">
-			<div class="row align-middle align-justify">
-              <?php print render($page['header-top-left']); ?>
-              <?php print render($page['header-search']); ?>
-				<div id="header-top-right">
-                  <?php print render($page['commerce-header']); ?>
+			<div class="container">
+				<div class="row align-middle align-justify">
+					<div id="header-top-left">
+	                  <?php print render($page['header-top-left']); ?>
+	                  <?php print render($page['header-search']); ?>
+					</div>
+					<div id="header-top-right">
+	                  <?php print render($page['header-top-right']); ?>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -17,20 +22,24 @@
 			<div class="title-bar">
 				<div class="title-bar-left">
 					<div class="mobile-buttons-left">
-						<button class="menu-icon" type="button" data-toggle="offCanvas"></button>
-						<button data-toggle="mobile-search-dropdown"><i class="fa fa-search"></i></button>
+						<button class="menu-icon" type="button"
+						        data-toggle="offCanvas"></button>
+						<button data-toggle="mobile-search-dropdown"><i
+							  class="fa fa-search"></i></button>
                       <?php print render($page['header-top-left']); ?>
 					</div>
 					<div class="mobile-buttons-right">
 						<a href="/user"><i class="fa fa-user"></i></a>
-						<a href="/cart"><i class="fa fa-shopping-cart"></i></a>
+						<a href="/cart"><i
+							  class="fa fa-shopping-cart"></i></a>
 					</div>
 				</div>
 
 			</div>
 			<!-- Mobile Search Dropdown -->
 			<div class="mobile-nav-search-wrap">
-				<div class="dropdown-pane" id="mobile-search-dropdown" data-dropdown data-auto-focus="true">
+				<div class="dropdown-pane" id="mobile-search-dropdown"
+				     data-dropdown data-auto-focus="true">
                   <?php print render($page['header-top-left']); ?>
 				</div>
 			</div>
@@ -40,7 +49,11 @@
 		<div id="logo-wrap">
 			<div class="row align-middle align-center">
               <?php if ($logo): ?>
-				  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" id="main-logo"/></a>
+				  <a href="<?php print $front_page; ?>"
+				     title="<?php print t('Home'); ?>" rel="home"
+				     class="site-logo"><img src="<?php print $logo; ?>"
+				                            alt="<?php print t('Home'); ?>"
+				                            id="main-logo"/></a>
               <?php endif; ?>
 
 			</div>
@@ -79,20 +92,13 @@
       <?php print render($page['navigation']); ?>
 	</div>
 
-	<!-- Mobile Nav Bar -->
-	<div class="off-canvas-content" data-off-canvas-content>
-		<div class="title-bar">
-			<div class="title-bar-left">
-				<button class="menu-icon" type="button" data-toggle="offCanvas"></button>
-				<!--          <span class="title-bar-title">Foundation title bar with top off-canvas</span>-->
-			</div>
-		</div>
-	</div>
 
 	<!-- Mobile Nav Hidden Menu -->
-	<div class="off-canvas position-left" id="offCanvas" data-off-canvas data-transition="overlap">
+	<div class="off-canvas position-left" id="offCanvas" data-off-canvas
+	     data-transition="overlap">
 		<!-- Close button -->
-		<button class="close-button" aria-label="Close menu" type="button" data-close>
+		<button class="close-button" aria-label="Close menu" type="button"
+		        data-close>
 			<span aria-hidden="true">&times;</span>
 		</button>
 		<!-- Menu -->
@@ -114,7 +120,8 @@
 
 			<!--Mobile products filter-->
 			<div id="mobile-sidebar-wrapper">
-				<button id="filter-button">Filter Catalog<i class="fa fa-caret-down" aria-hidden="true"></i></button>
+				<button id="filter-button">Filter Catalog<i
+					  class="fa fa-caret-down" aria-hidden="true"></i></button>
 				<div id="sidebar-first-collapsible" class="filter-products">
                   <?php print render($page['sidebar_first']); ?>
 				</div>
@@ -131,7 +138,8 @@
               <?php print render($tabs); ?>
               <?php print render($page['help']); ?>
               <?php if ($action_links): ?>
-				  <ul class="action-links"><?php print render($action_links); ?></ul>
+				  <ul
+					class="action-links"><?php print render($action_links); ?></ul>
               <?php endif; ?>
               <?php print render($page['content']); ?>
               <?php print $feed_icons; ?>
@@ -141,6 +149,7 @@
 	</div>
 
 	<footer id="footer-wrap">
+		<div class="container">
 		<div class="row">
           <?php print render($page['footer_first']); ?>
           <?php print render($page['footer_second']); ?>
@@ -152,8 +161,11 @@
               <?php if (!empty(variable_get('site_name'))): ?>
                 <?php print variable_get('site_name'); ?>
               <?php endif; ?>
-				<br/>Website designed by <a href="http://www.completewebsol.com">Complete Web Solutions</a>
+				<br/>Website designed by <a
+				  href="http://www.completewebsol.com">Complete Web
+					Solutions</a>
 			</div>
+		</div>
 		</div>
 	</footer>
 </div>
